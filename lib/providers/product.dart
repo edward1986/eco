59807a555af0e27c716d00cf4ff092811baf3eef
Lorilla Family <edwardlorilla1986@gmail.com>
2021-07-24 10:28:43 +0800

    first commit

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -29,7 +29,7 @@ class Product with ChangeNotifier {
     final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
-    final url = 'https://flutter-update.firebaseio.com/products/$id.json';
+    final url = Uri.https('flutter-update.firebaseio.com', '/products/$id.json');
     try {
       final response = await http.patch(
         url,
